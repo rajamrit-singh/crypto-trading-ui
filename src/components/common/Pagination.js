cb9a4720da44acf37017ff8a166224083d1252d8
rajamrit-singh <rajamritsingh1996@gmail.com>
2023-07-27 00:39:27 -0700

    refactored user navbar and pagination

--- src/components/common/Pagination.js
+++ src/components/common/Pagination.js
@@ -4,38 +4,54 @@ import { Pagination } from 'react-bootstrap';
 const PaginationComponent = ({ currentPage, updatePage, stats }) => {
     const [pagesToShow, setPagesToShow] = useState([]);
     const lastPage = stats ? Math.ceil(stats?.totalCoins / 50) : 5;
-    console.log(currentPage);
+
     useEffect(() => {
         let initial;
         let last;
-            const pagesToShow = [1]
-            if(currentPage <=4 ) {
-                initial = 2;
-                last = 5;
-            } else if (currentPage >= lastPage - 3) {
-                initial = lastPage - 3;
-                last = lastPage;
-            } else {
-                initial = currentPage - 1;
-                last = lastPage
-            }
-            for (let i = initial; i < initial + 3; i++) {
-                pagesToShow.push(i);
-            }
-            pagesToShow.push(last);
-            setPagesToShow(pagesToShow);
+        const pagesToShow = [1];
+
+        if (currentPage <= 3) {
+            initial = 2;
+            last = lastPage;
+        } else if (currentPage >= lastPage - 3) {
+            initial = lastPage - 3;
+            last = lastPage;
+        } else {
+            initial = currentPage - 1;
+            last = lastPage;
+        }
+
+        for (let i = initial; i < initial + 3; i++) {
+            pagesToShow.push(i);
+        }
+
+        pagesToShow.push(last);
+        setPagesToShow(pagesToShow);
     }, [currentPage, lastPage]);
+
     return (
         <Pagination>
-            {pagesToShow.map((page) => 
-            <Pagination.Item
-                key={page}
-                active={currentPage === page}
-                onClick={updatePage.bind(this, page)}
-            >{page}</Pagination.Item>
-            )}
+            <Pagination.Prev
+                onClick={updatePage.bind(this, currentPage - 1)}
+                disabled={currentPage === 1}
+            />
+
+            {pagesToShow.map((page) => (
+                <Pagination.Item
+                    key={page}
+                    active={currentPage === page}
+                    onClick={updatePage.bind(this, page)}
+                >
+                    {page}
+                </Pagination.Item>
+            ))}
+
+            <Pagination.Next
+                onClick={updatePage.bind(this, currentPage + 1)}
+                disabled={currentPage === lastPage}
+            />
         </Pagination>
-    )
-}
+    );
+};
 
 export default PaginationComponent;
--- src/components/layout/UserNavbar.js
+++ src/components/layout/UserNavbar.js
@@ -1,37 +1,67 @@
-import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Navbar, Nav, Container, Dropdown } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { BsCoin } from 'react-icons/bs';
-import { changeCurrentTabAndNavigateTo } from '../../utils/navigationUtils';
+import { changeCurrentTabAndNavigateTo, getUserIdFromUrl } from '../../utils/navigationUtils';
+import { CgProfile } from 'react-icons/cg';
+import './UserNavbar.css';
+
 
 const UserNavbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const balance = '$1000';
   const navigate = useNavigate();
 
   const goToTab = (routeName) => {
-    const newUrl = changeCurrentTabAndNavigateTo(routeName);
-    navigate(newUrl);
+    const id = getUserIdFromUrl();
+    navigate(`/${id}${routeName}`);
   };
+
+  const handleLogOut = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    navigate(`/`);
+  }
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
+      <Container id="custom-container">
         <Navbar.Brand>
           <BsCoin />
         </Navbar.Brand>
-        <Navbar.Toggle  area-controls="home-navbar"/> {/* area-controls needs the id of the navbar it is trying to collapse*/}
+        <Navbar.Toggle area-controls="home-navbar" />
         <Navbar.Collapse id="home-navbar">
           <Nav>
             <Nav.Link onClick={() => goToTab('/home')}>Home</Nav.Link>
             <Nav.Link onClick={() => goToTab('/sell')}>Sell</Nav.Link>
             <Nav.Link onClick={() => goToTab('/transactions')}>Transactions</Nav.Link>
           </Nav>
-          <Nav>
-            <Nav.Link as={Link} to="/logout">
-              Logout
-            </Nav.Link>
-          </Nav>
         </Navbar.Collapse>
       </Container>
+      <Nav className="icon-container">
+        {/* User Balance */}
+        <Nav.Link className="balance">
+          Balance: <span className="balance-amount">{balance}</span>
+        </Nav.Link>
+        <Nav.Link
+          className="profile-icon"
+          onClick={() => setShowDropdown((prev) => !prev)}
+        >
+          <CgProfile size={25} />
+        </Nav.Link>
+        {showDropdown && (
+          <Dropdown className="profile-dropdown" align="end" show={showDropdown}>
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => console.log('My Profile Clicked')}>
+                My Profile
+              </Dropdown.Item>
+              <Dropdown.Item onClick={handleLogOut}>
+                Logout
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        )}
+      </Nav>
     </Navbar>
   );
 };
